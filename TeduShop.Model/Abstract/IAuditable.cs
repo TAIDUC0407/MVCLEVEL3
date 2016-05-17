25b7d1e4f3b843ed84daba11449f6f4bb07e9ec8
DUCTAI <DUCTAI@TAIDUC>
2016-05-17 22:25:17 +0700

    end lesson 5

---
+++ TeduShop.Model/Abstract/IAuditable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TeduShop.Model.Abstract
+{
+    public interface IAuditable
+    {
+        DateTime? CreatedDate { set; get; }
+        string CreatedBy { set; get; }
+        DateTime? UpdatedDate { set; get; }
+        string UpdatedBy { set; get; }
+
+        string MetaKeyword { get; set; }
+        string MetaDescription { get; set; }
+
+        bool Status { get; set; }
+
+
+    }
+}
