7cd594a9d13ece8f988af142925dade567a11a5e
DUCTAI <DUCTAI@TAIDUC>
2016-05-26 11:04:18 +0700

    add2

--- TeduShop.Model/Models/Post.cs
+++ TeduShop.Model/Models/Post.cs
@@ -42,7 +42,7 @@ namespace TeduShop.Model.Models
         [ForeignKey("CategoryID")]
         public virtual PostCategory PostCategory { set; get; }
 
-        //[ForeignKey("CategoryID")]
+        [ForeignKey("CategoryID")]
         public virtual IEnumerable<PostTag> PostTags { set; get; }
     }
 }
