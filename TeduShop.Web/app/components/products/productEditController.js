eaef87cd658488bcaa45104a870f6cf9ed11a9f1
DUCTAI <DUCTAI@TAIDUC>
2016-05-28 18:31:56 +0700

    router implement for app

--- TeduShop.Web/app/components/products/productEditController.js
+++ TeduShop.Web/app/components/products/productEditController.js
@@ -1,1 +1,8 @@
-﻿
+﻿(function (app) {
+    app.controller('productEditController', productEditController);
+
+    function productEditController() {
+
+    }
+
+})(angular.module('tedushop.products'));
